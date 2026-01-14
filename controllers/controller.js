2fd3d22d06d04723328be9bfc3b7c81700a7d856
GOVIND <work.govindsahu@gmail.com>
2026-01-14 10:07:11 +0530

    add spawnChildProcess function

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,11 @@
-import { spawn } from "child_process";
-import fs from "fs/promises";
-import { prepareScript } from "../utils/utils.js";
-import { setGithubStatus } from "../services/statusApis.js";
-import { sendEmail } from "../services/sendEmail.js";
-import { checkHealth, cleanupDisk, triggerRollback } from "../utils/utils.js";
+import {
+  onCloseForStartingScript,
+  onErrorForStartingScript,
+  prepareScript,
+  spawnChildProcess,
+  stderrOnDataForStartingScript,
+  stdoutOnDataForStartingScript,
+} from "../utils/utils.js";
 
 export const serverController = async (req, res, next) => {
   console.log("webhook started!");
@@ -13,67 +15,14 @@ export const serverController = async (req, res, next) => {
 
     await prepareScript(req);
 
-    const bashChildProcess = spawn("bash", [`./${sha}.sh`]);
-
-    bashChildProcess.stdout.on("data", async (data) => {
-      process.stdout.write(data);
-      await fs.appendFile("./logs/logs.txt", data.toString(), "utf8");
-    });
-
-    bashChildProcess.stderr.on("data", async (data) => {
-      process.stderr.write(data);
-      await fs.writeFile("./logs/logs.txt", data.toString(), "utf8");
-    });
-
-    bashChildProcess.on("close", async (code) => {
-      if (code === 0) {
-        const isHealthy = await checkHealth(
-          "https://test.api.govindsahu.me/health"
-        );
-
-        if (isHealthy) {
-          await setGithubStatus(
-            repositoryName,
-            sha,
-            "success",
-            "Build and deployed succefull!",
-            "http://localhost:4000/logs.txt"
-          );
-
-          await cleanupDisk(req);
-
-          console.log("Script executed successfully!");
-        } else {
-          console.log("🚨 App is unhealthy! Triggering automatic rollback...");
-          await triggerRollback(req);
-        }
-      } else {
-        await setGithubStatus(
-          repositoryName,
-          sha,
-          "failure",
-          `Failed to run pipeline for commit ${req.body.commits[0].message}`,
-          "http://localhost:4000/logs.txt"
-        );
-        await sendEmail(req.body);
-        await fs.rm(`${sha}.sh`);
-        console.log("Script execution failed!");
-      }
-    });
-
-    bashChildProcess.on("error", async (err) => {
-      await setGithubStatus(
-        repositoryName,
-        sha,
-        "failure",
-        `Failure ${err.message}`,
-        "http://localhost:4000/logs.txt"
-      );
-      await sendEmail(req.body);
-      await fs.rm(`${sha}.sh`);
-      console.log("Error in spawning the process!");
-      console.log(err);
-      await fs.writeFile("./logs/logs.txt", err.toString(), "utf8");
+    await spawnChildProcess({
+      sha,
+      stdoutOnData: (data) => stdoutOnDataForStartingScript(data),
+      stderrOnData: (data) => stderrOnDataForStartingScript(data),
+      onClose: (code) =>
+        onCloseForStartingScript({ code, req, sha, repositoryName }),
+      onError: (err) =>
+        onErrorForStartingScript({ err, req, sha, repositoryName }),
     });
   } catch (error) {
     next(error);
--- utils/utils.js
+++ utils/utils.js
@@ -3,6 +3,7 @@ import { spawn } from "child_process";
 import fs from "fs/promises";
 import { setGithubStatus } from "../services/statusApis.js";
 import yaml from "js-yaml";
+import { sendEmail } from "../services/sendEmail.js";
 
 // Helper function to verify the app is healthy
 export const checkHealth = async (url, retries = 5) => {
@@ -148,4 +149,91 @@ export const prepareBashFile = async (req) => {
   return { environment };
 };
 
-export const spawnChildProcess = async () => {};
+export const stdoutOnDataForStartingScript = async (data) => {
+  process.stdout.write(data);
+  await fs.appendFile("./logs/logs.txt", data.toString(), "utf8");
+};
+
+export const stderrOnDataForStartingScript = async (data) => {
+  process.stderr.write(data);
+  await fs.writeFile("./logs/logs.txt", data.toString(), "utf8");
+};
+
+export const onCloseForStartingScript = async ({
+  code,
+  req,
+  sha,
+  repositoryName,
+}) => {
+  if (code === 0) {
+    const isHealthy = await checkHealth(
+      "https://test.api.govindsahu.me/health"
+    );
+
+    if (isHealthy) {
+      await setGithubStatus(
+        repositoryName,
+        sha,
+        "success",
+        "Build and deployed succefull!",
+        "http://localhost:4000/logs.txt"
+      );
+
+      await cleanupDisk(req);
+
+      console.log("Script executed successfully!");
+    } else {
+      console.log("🚨 App is unhealthy! Triggering automatic rollback...");
+      await triggerRollback(req);
+    }
+  } else {
+    await setGithubStatus(
+      repositoryName,
+      sha,
+      "failure",
+      `Failed to run pipeline for commit ${req.body.commits[0].message}`,
+      "http://localhost:4000/logs.txt"
+    );
+    await sendEmail(req.body);
+    await fs.rm(`${sha}.sh`);
+    console.log("Script execution failed!");
+  }
+};
+
+export const onErrorForStartingScript = async ({
+  err,
+  req,
+  sha,
+  repositoryName,
+}) => {
+  await setGithubStatus(
+    repositoryName,
+    sha,
+    "failure",
+    `Failure ${err.message}`,
+    "http://localhost:4000/logs.txt"
+  );
+  await sendEmail(req.body);
+  await fs.rm(`${sha}.sh`);
+  console.log("Error in spawning the process!");
+  console.log(err);
+  await fs.writeFile("./logs/logs.txt", err.toString(), "utf8");
+};
+
+export const spawnChildProcess = async ({
+  sha,
+  stdoutOnData,
+  stderrOnData,
+  onClose,
+  onError,
+}) => {
+  const bashChildProcess = spawn("bash", [`./${sha}.sh`]);
+
+  bashChildProcess.stdout.on("data", async (data) => stdoutOnData(data));
+
+  bashChildProcess.stderr.on("data", async (data) => stderrOnData(data));
+
+  bashChildProcess.on("close", async (code) => onClose(code));
+
+  bashChildProcess.on("error", async (err) => onError(err));
+};
